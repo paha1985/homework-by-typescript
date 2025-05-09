a2d96a7b3c9a461493018b4ca3308bed0c9d3287
paha1985 <paha290585@gmail.com>
2025-05-09 11:41:15 +0800

    complete task-2

--- task-1.js
+++ task-1.js
@@ -1,12 +1,7 @@
 "use strict";
 const totalPrice = ({ price, discount, isInstallment, months }) => {
-  const priceWithDiscount = (price * (1 - discount / 100)) / months;
-  return isInstallment ? priceWithDiscount : price;
+    const priceWithDiscount = price * (1 - discount / 100) / months;
+    return isInstallment ? priceWithDiscount : price;
 };
-const price = totalPrice({
-  price: 100000,
-  discount: 25,
-  isInstallment: true,
-  months: 12,
-});
+const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
 console.log(price); // 6250
