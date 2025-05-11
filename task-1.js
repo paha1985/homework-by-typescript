a4edaecd0772056660bf0a81905cf2e83d41cb9c
paha1985 <paha290585@gmail.com>
2025-05-11 15:45:22 +0800

    fix: delete js files from tasks

--- task-1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const totalPrice = ({ price, discount, isInstallment, months }) => {
-    const priceWithDiscount = price * (1 - discount / 100) / months;
-    return isInstallment ? priceWithDiscount : price;
-};
-const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
-console.log(price); // 6250
